9bcda0a7c70c20d87e65c953966d7d3886b9bdfd
SreejayakowsalyaS <47312612+SreejayakowsalyaS@users.noreply.github.com>
2019-02-22 14:36:44 +0530

    Create Mutable.java

---
+++ Mutable.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Mutable{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int[] arr=new int[]{1,2,3};
+arr[1]=10;
+for(int i=0;i<arr.length;i++){
+System.out.println(arr[i]);
+}
+}
+}
