a0126f32df040c74573f2dfe5c9259a3811b22e7
SreejayakowsalyaS <47312612+SreejayakowsalyaS@users.noreply.github.com>
2019-08-02 13:56:46 +0530

    Add files via upload

---
+++ BiggerNum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class BiggerNum{
+public static void main(String args[]){
+int x=50,y=25;
+if(x>y)
+System.out.println("the no "+x+" is big");
+else
+System.out.println("the no "+y+" is big");
+System.out.println("thank you");
+}
+}
